163572347933650aced26e8cb7be52a7732fdf49
Den_Blazh <denisb53502006@gmail.com>
2025-03-24 11:08:38 +0200

    Lab2 commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './home/content/content.component';
+
+
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent, children: [
+      { path: 'dashboard', component: ContentComponent },
+      { path: 'profile', component: ContentComponent },
+      { path: 'settings', component: ContentComponent },
+    ]}
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [HeaderComponent, SidebarComponent, ContentComponent] // Додаємо вкладені компоненти
 })
 export class HomeComponent {
+  contentText = 'Більше...';
 
+  updateContent(newContent: string) {
+    this.contentText = newContent;
+  }
 }
