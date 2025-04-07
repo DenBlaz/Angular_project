4b2f99599d0255469ec8953753b3e76063555f6e
Den_Blazh <denisb53502006@gmail.com>
2025-04-07 11:43:04 +0300

    Lab3 commit

--- src/app/home/content/content.component.ts
+++ src/app/home/content/content.component.ts
@@ -1,14 +1,64 @@
-import { Component, Input } from '@angular/core';
+import {Component, inject, Input, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../../api.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule]
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
   items = ['Кактус', 'Банан', 'Гуава', 'Ківі'];
   @Input() content: string = 'Оберіть розділ';
+
+  data: any[] = [];
+  error: string = '';
+  postSuccess = false;
+
+  newData = {
+    title: '',
+    body: ''
+  };
+
+  private apiService = inject(ApiService);
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const stored = localStorage.getItem('userPosts');
+      if (stored) {
+        this.data = JSON.parse(stored);
+      }
+    }
+  }
+
+  fetchData(): void {
+    this.apiService.getData().subscribe({
+      next: (res) => {
+        this.data = res;
+        this.error = '';
+      },
+      error: (err) => {
+        this.error = err.message;
+      }
+    });
+  }
+
+  submitForm(): void {
+    const newEntry = { ...this.newData };
+
+    this.data.unshift(newEntry); // додаємо новий запис на початок
+
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.setItem('userPosts', JSON.stringify(this.data));
+    }
+
+    this.postSuccess = true;
+    this.error = '';
+    this.newData = { title: '', body: '' };
+  }
+
 }
